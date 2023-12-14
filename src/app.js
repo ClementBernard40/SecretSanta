9982906ec8e101bbb5d1e6bded36469bb02e57bb
clement bernard <c.bernard817@laposte.net>
2023-12-14 16:12:43 +0100

    add user login, user register, user update, user delete

--- src/app.js
+++ src/app.js
@@ -1,1 +1,31 @@
-init
+const express = require('express');
+const app = express();
+const port = 3000;
+const host = '0.0.0.0';
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/SecretSanta");
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+// const swaggerUi = require('swagger-ui-express');
+// const swaggerDocument = require('./swagger.json');
+
+// router.use('/api-docs', swaggerUi.serve);
+// router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
+
+
+
+
+// const postRoute = require('./routes/postRoute');
+//const timerRoute = require('./routes/timerRoute');
+const userRoute = require('./routes/userRoute');
+
+// app.use('/posts', postRoute);
+app.use('/', timerRoute);
+app.use('/users', userRoute);
+
+
+app.listen(port, host);
